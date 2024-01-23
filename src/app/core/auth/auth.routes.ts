4072b1eab3c8bf4a8c153d54c170fc8fb067b466
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2024-01-23 02:00:01 +0100

    Refactor

--- src/app/core/auth/auth.routes.ts
+++ src/app/core/auth/auth.routes.ts
@@ -2,5 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 
 export const AUTH_ROUTES: Routes = [
-  { path: 'login', component: LoginComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
 ];
